263e6fa57acb35687a40ada29d51815d4c885015
Neal Riley <neal.riley@gmail.com>
2024-05-28 11:16:04 +0000

    cleanliness is godliness

--- app/streamlit-p5.py
+++ app/streamlit-p5.py
@@ -27,6 +27,14 @@ st.header("Streamlit and Processing together at last")
 
 """Both Streamlit and Processing excel in their own areas, but together they compliment each other perfectly. Streamlit, and the power of Python (and integrations with Snowflake, for example), can do some awesome things with relatively minimal code. Processing does the same, with an awesome interactive capability, driven by Javascript, the HTML canvas, and a library called P5.js."""
 
+with st.sidebar:
+    st.header("Links")
+
+    st.write("""
+    - [pypi.org/project/streamlit-p5](https://pypi.org/project/streamlit-p5/)
+    - [github.com/salable/streamlit-p5](https://github.com/salable/streamlit-p5)
+    """)
+
 st.header("What is Processing?")
 
 """Processing (also known as Processing.org, P5, or P5.js) started as the brainchild of Ben Fry and Case Reas. Back in the early 2000s this manifest itself as a Java applet and language base, but has grown over the years. Check them out at [Processing.org](https://www.processing.org) and the Javascript/HTML5 offshot [P5.js](https://p5js.org/).
